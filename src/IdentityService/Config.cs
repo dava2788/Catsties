c091ad73a53ebb7495a615f0d5658bee92749540
Dava2788 <dava2788@gmail.com>
2023-09-14 17:42:11 -0600

    End Section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -1,7 +1,5 @@
-﻿using Duende.IdentityServer.Models;
-
+﻿﻿using Duende.IdentityServer.Models;
 namespace IdentityService;
-
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
@@ -10,13 +8,11 @@ public static class Config
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
         };
-
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("auctionApp","Auction app full access")
+            new ApiScope("auctionApp", "Auction app full access"),
         };
-
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
@@ -28,6 +24,18 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
             }
         };
 }
